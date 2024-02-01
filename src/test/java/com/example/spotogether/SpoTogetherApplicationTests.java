f3e6cdd7530dce38856cf380d6c823d61ab22195
Sunho1998 <fif55@naver.com>
2024-02-01 14:16:52 +0900

    다운그레이드 프로젝트 생성

---
+++ src/test/java/com/example/spotogether/SpoTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spotogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpoTogetherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
